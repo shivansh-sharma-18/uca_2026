73c5ae38b232c44b85ad7c988242879fd3e4bca3
shivansh-sharma-18 <shivanshsharma200618@gmail.com>
2026-07-31 15:45:57 +0530

    Addes the fitsBits program

---
+++ Week_4/Bits_And_Bytes/fits_bits.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int fitsBits(int x, int n) {
+  int shift = 32 + (~n + 1); // 32 - n
+  return !(((x << shift) >> shift) ^ x);
+}
+
+int main() {
+  int x, n;
+
+  printf("Enter x: ");
+  scanf("%d", &x);
+
+  printf("Enter n: ");
+  scanf("%d", &n);
+
+  printf("%d\n", fitsBits(x, n));
+
+  return 0;
+}
