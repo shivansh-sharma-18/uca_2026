2dc88af60339945f082d6ad7ab16de64f9489d7a
shivansh-sharma-18 <shivanshsharma200618@gmail.com>
2026-08-10 17:45:47 +0530

    Added the decode part

--- Week_5/base64_encoding.c
+++ Week_5/base64_encoding.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+char table[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 void base64Encode(char input[]) {
-  char table[] =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
   int len = strlen(input);
 
   for (int i = 0; i < len; i += 3) {
@@ -33,13 +34,71 @@ void base64Encode(char input[]) {
   printf("\n");
 }
 
+int getValue(char ch) {
+  if (ch >= 'A' && ch <= 'Z')
+    return ch - 'A';
+
+  if (ch >= 'a' && ch <= 'z')
+    return ch - 'a' + 26;
+
+  if (ch >= '0' && ch <= '9')
+    return ch - '0' + 52;
+
+  if (ch == '+')
+    return 62;
+
+  if (ch == '/')
+    return 63;
+
+  return -1;
+}
+
+void base64Decode(char input[]) {
+  int len = strlen(input);
+
+  for (int i = 0; i < len; i += 4) {
+    int a = getValue(input[i]);
+    int b = getValue(input[i + 1]);
+    int c = (input[i + 2] == '=') ? 0 : getValue(input[i + 2]);
+    int d = (input[i + 3] == '=') ? 0 : getValue(input[i + 3]);
+
+    int x = (a << 2) | (b >> 4);
+    int y = ((b & 15) << 4) | (c >> 2);
+    int z = ((c & 3) << 6) | d;
+
+    printf("%c", x);
+
+    if (input[i + 2] != '=')
+      printf("%c", y);
+
+    if (input[i + 3] != '=')
+      printf("%c", z);
+  }
+
+  printf("\n");
+}
+
 int main() {
   char input[1000];
+  int choice;
 
+  printf("1. Encode\n");
+  printf("2. Decode\n");
+  printf("Enter choice: ");
+  scanf("%d", &choice);
+
+  getchar();
+
+  printf("Enter input: ");
   fgets(input, sizeof(input), stdin);
   input[strcspn(input, "\n")] = '\0';
 
-  base64Encode(input);
+  if (choice == 1)
+    base64Encode(input);
+  else if (choice == 2)
+    base64Decode(input);
+  else
+    printf("Invalid choice\n");
 
   return 0;
 }
